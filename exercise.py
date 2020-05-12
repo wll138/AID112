cc3c7e60450f88d9565d778a3e5833516762e5e2
wll <1121162538@qq.com>
2020-05-12 13:29:14 +0800

    add exercise

---
+++ exercise.py
@@ -0,0 +1,22 @@
+"""
+1.排除下列代码错误：
+name = input("请输入姓名：")
+age = input("请输入年龄：")
+print(输入的姓名是：name)
+print("输入的年龄是：age")
+
+2.画出下列代码内存图：
+count = 100
+sum = count + 5
+count = 0
+
+"""
+
+name = input("请输入姓名：")
+age = input("请输入年龄：")
+print("你输入的姓名是：" + name)
+print("你输入的年龄是：" + age)
+
+# count = 100
+# sum = count + 5
+# print(sum)
